544c4e9b77d43134da5ecf60006bcf53415a99ab
Eva <evamayser04@gmail.com>
2026-03-26 13:21:57 +0100

    COMITEADO

---
+++ figuras/Cuadrado.java
@@ -0,0 +1,10 @@
+package figuras;
+
+import java.awt.Color;
+
+public class Cuadrado extends Rectangulo {
+	public Cuadrado(double x, double y, Color color, double lado) {
+		super(x, y, color, lado, lado);
+	}
+}
+//clase modificada para la rama develop
